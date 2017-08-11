b522253668adfc4512ce6788574e3ff4bea84eec
Beno Suriano <bjsuriano@gmail.com>
2017-08-11 09:59:48 -0600

    Third commit.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,7 +1,7 @@
-const blankLine = ' | | ';
+// Run node minesweeper.js to run this code.
 
+const blankLine = '  |   |  ';
 console.log('This is what an empty board would look like:');
-
-console.log(${blankLine});
-console.log(${blankLine});
-console.log(${blankLine});
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
